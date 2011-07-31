b5515543b5f1b2cda869df59f1caf1ed10e8b394
Jilles van Gurp <incoming@jillesvangurp.com>
2011-07-31 14:31:37 +0200

    Documentation

--- src/main/java/com/github/jsonj/package-info.java
+++ src/main/java/com/github/jsonj/package-info.java
@@ -1,5 +1,7 @@
 /**
- * Classes that represent json in Java. 
+ * Classes that represent json in Java. There are three classes: JsonObject, JsonArray, and JsonPrimitive. They each implement the JsonElement interface.
+ * Additionally, JsonObject and JsonArray extend the Java collections framework. Consequently, you can use them as a <code>Map&lt;String, JsonElement&gt;</code> and
+ * <code>List&lt;JsonElement&gt;</code> respectively.
  */
 package com.github.jsonj;
 
