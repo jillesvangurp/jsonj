bc3f83426a9c5fd3299b00698573559c2bed1f92
Jilles van Gurp <jilles@jillesvangurp.com>
2013-01-02 17:22:12 +0100

    add proper geojson style support for polygons

--- src/main/java/com/github/jsonj/tools/TypeSupport.java
+++ src/main/java/com/github/jsonj/tools/TypeSupport.java
@@ -12,23 +12,47 @@ import com.github.jsonj.JsonArray;
 public class TypeSupport {
 
     /**
-     * Convert 2d array of doubles to the json equivalent. Useful for polygons.
-     * @param twoDArray
+     * Convert 2d array of doubles to a geojson style multi polygon in a 3D array of arrays of [latitude,longitude]
+     * arrays.
+     *
      * @return a JsonArray
      */
-    public static JsonArray convertFrom2DDoubleArray(double[][] twoDArray) {
-        JsonArray result = array();
-        for (int i=0;i<twoDArray.length ; i++) {
-            double[] values = twoDArray[i];
+    public static JsonArray getGeoJsonPolygon(double[][] coordinates) {
+        JsonArray points = array();
+        for (int i=0;i<coordinates.length ; i++) {
+            double[] values = coordinates[i];
             JsonArray subArray = array();
             for (int j = 0; j < values.length; j++) {
                 subArray.add(primitive(values[j]));
             }
-            result.add(subArray);
+            points.add(subArray);
+        }
+        if(coordinates[0][0] != coordinates[coordinates.length-1][0] || coordinates[0][1] != coordinates[coordinates.length-1][1]) {
+            // add last coordinate to close the polygon
+            points.add(array(coordinates[0][0], coordinates[0][1]));
+        }
+
+        JsonArray geoJsonPolygon = array();
+        geoJsonPolygon.add(points);
+        return geoJsonPolygon;
+    }
+
+    /**
+     * @param geoJsonMultiPolygon
+     * @return the 2d array of coordinates for the first polygon in the GeoJson multipolygon.
+     */
+    public static double[][] fromGeoJsonPolygon(JsonArray geoJsonMultiPolygon) {
+        JsonArray firstPolygon = geoJsonMultiPolygon.get(0).asArray();
+        double[][] result = new double[firstPolygon.size()][firstPolygon.get(0).asArray().size()];
+        for(int i =0; i<firstPolygon.size();i++) {
+            JsonArray subArray = firstPolygon.get(i).asArray();
+            for(int j =0; j<subArray.size();j++) {
+                result[i][j] = subArray.get(j).asPrimitive().asDouble();
+            }
         }
         return result;
     }
-    
+
     /**
      * Convert json array of arrays of double primitives to a double[][].
      * @param arrayOfArrays
@@ -40,8 +64,8 @@ public class TypeSupport {
             JsonArray subArray = arrayOfArrays.get(i).asArray();
             for(int j =0; j<subArray.size();j++) {
                 result[i][j] = subArray.get(j).asPrimitive().asDouble();
-            }            
-        }        
+            }
+        }
         return result;
     }
 }
