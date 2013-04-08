c5162145650d9eec1d80aab4373453c8b0a30e4a
Jilles van Gurp <jilles@jillesvangurp.com>
2013-04-08 15:54:22 +0200

    add JsonSet implementation that extends JsonArray

---
+++ src/main/java/com/github/jsonj/JsonSet.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2011, Jilles van Gurp
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.jsonj;
+
+import static com.github.jsonj.tools.JsonBuilder.fromObject;
+import static com.github.jsonj.tools.JsonBuilder.primitive;
+
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * Representation of json arrays that behaves like a set.
+ */
+@SuppressWarnings("unchecked")
+public class JsonSet extends JsonArray implements Set<JsonElement> {
+    private static final long serialVersionUID = 753773658521455994L;
+
+    public JsonSet() {
+	    super();
+	}
+
+    @SuppressWarnings("rawtypes")
+    public JsonSet(Collection existing) {
+        super();
+        for(Object o: existing) {
+            JsonElement fromObject = fromObject(o);
+            if(!contains(fromObject)) {
+                add(fromObject);
+            }
+        }
+    }
+
+
+    @SuppressWarnings("rawtypes")
+    public JsonSet(Set existing) {
+        super(existing);
+    }
+
+    /**
+     * Variant of add that takes a string instead of a JsonElement. The inherited add only supports JsonElement.
+     * @param s
+     */
+    @Override
+    public void add(final String s) {
+        JsonPrimitive primitive = primitive(s);
+        if(!contains(primitive)) {
+            add(primitive);
+        }
+    }
+
+    /**
+     * Variant of add that adds multiple strings.
+     * @param strings
+     */
+    @Override
+    public void add(final String...strings) {
+        for (String s : strings) {
+            JsonPrimitive primitive = primitive(s);
+            if(!contains(primitive)) {
+                add(primitive);
+            }
+        }
+    }
+
+    /**
+     * Variant of add that adds multiple JsonElements.
+     * @param elements
+     */
+    @Override
+    public void add(final JsonElement...elements) {
+        for (JsonElement element : elements) {
+            JsonPrimitive primitive = primitive(element);
+            if(!contains(primitive)) {
+                add(primitive);
+            }
+        }
+    }
+
+    @Override
+    public boolean addAll(@SuppressWarnings("rawtypes") Collection c) {
+        for (Object element : c) {
+            if(element instanceof JsonElement) {
+                if(!contains(element)) {
+                    add((JsonElement)element);
+                }
+            } else {
+                JsonPrimitive primitive = primitive(element);
+                if(!contains(primitive)) {
+                    add(primitive);
+                }
+            }
+        }
+        return c.size() != 0;
+    }
+
+}
---
+++ src/test/java/com/github/jsonj/JsonSetTest.java
@@ -0,0 +1,48 @@
+package com.github.jsonj;
+
+import static com.github.jsonj.tools.JsonBuilder.array;
+import static com.github.jsonj.tools.JsonBuilder.primitive;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+@Test
+public class JsonSetTest {
+    public void shouldAddNoDuplicateStrings() {
+        JsonSet set = new JsonSet();
+        set.add("42");
+        set.add("42");
+        assertThat(set.size(), is(1));
+    }
+
+    public void shouldAddNoDuplicateStringsVarargs() {
+        JsonSet set = new JsonSet();
+        set.add("42", "42", "43");
+        assertThat(set.size(), is(2));
+    }
+
+    public void shouldAddNoDuplicatePrimitives() {
+        JsonSet set = new JsonSet();
+        set.add(primitive("42"), primitive("42"), primitive("43"));
+        assertThat(set.size(), is(2));
+    }
+
+    public void shouldAddNoDuplicatesFromCollection() {
+        List<Serializable> collection = Arrays.asList("42",primitive("42"), array(42),array(42));
+
+        JsonSet set = new JsonSet();
+        set.addAll(collection);
+        assertThat(set.size(), is(2));
+    }
+
+    public void shouldAddNoDuplicatesOnConstruction() {
+        List<Serializable> collection = Arrays.asList("42",primitive("42"), array(42),array(42));
+        JsonSet set = new JsonSet(collection);
+        assertThat(set.size(), is(2));
+    }
+}
