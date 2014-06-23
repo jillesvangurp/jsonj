560f2f18163893f60320761f7f78e232a24c7f47
Jilles van Gurp <jilles@jillesvangurp.com>
2014-06-23 11:48:33 +0200

    update instructions

--- deploytosonatype.sh
+++ deploytosonatype.sh
@@ -3,8 +3,7 @@
 # automated way with staging plugin thingy:
 # mvn release:prepare
 # mvn release:perform
-# mvn nexus-staging:release -Ddescription="Release 1.33"
-# Or if that fails, figure out the id from the ui https://github.com/sonatype/nexus-maven-plugins/issues/50
+
+# figure out the id from the release:perform build log
 # mvn nexus-staging:release -Ddescription="Release 1.33" -DstagingRepositoryId=comjillesvangurp-1002
-
-
+# or do it manually here https://oss.sonatype.org/#stagingRepositories
