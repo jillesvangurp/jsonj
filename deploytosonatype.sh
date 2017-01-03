54b204bfacac01f34ac0666ece2c0041bbb6f747
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2017-01-03 09:22:27 +0100

    No longer needed

--- deploytosonatype.sh
+++
@@ -1,9 +0,0 @@
-
-
-# automated way with staging plugin thingy:
-# mvn release:prepare
-# mvn release:perform
-
-# figure out the id from the release:perform build log
-# mvn nexus-staging:release -Ddescription="Release 1.33" -DstagingRepositoryId=comjillesvangurp-1002
-# or do it manually here https://oss.sonatype.org/#stagingRepositories
