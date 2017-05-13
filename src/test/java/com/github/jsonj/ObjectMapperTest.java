80f9dc3053edd9cee80d754da93fc312f76567db
Jilles van Gurp <jilles@jillesvangurp.com>
2017-05-13 13:12:50 +0200

    test mixing pojos with jsonj

--- src/test/java/com/github/jsonj/ObjectMapperTest.java
+++ src/test/java/com/github/jsonj/ObjectMapperTest.java
@@ -6,6 +6,7 @@ import static com.github.jsonj.tools.JsonBuilder.fromObject;
 import static com.github.jsonj.tools.JsonBuilder.object;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.jsonj.tools.JsonParser;
 import java.io.IOException;
@@ -69,5 +70,50 @@ public class ObjectMapperTest {
             assertThat(e).isEqualTo(element);
         }
 
+    }
+
+    class Foo {
+        private String attribute;
+        private int anotherAttribute;
+        private JsonObject nestedJsonj;
+
+        public String getAttribute() {
+            return attribute;
+        }
+        public void setAttribute(String attribute) {
+            this.attribute = attribute;
+        }
+        public int getAnotherAttribute() {
+            return anotherAttribute;
+        }
+        public void setAnotherAttribute(int anotherAttribute) {
+            this.anotherAttribute = anotherAttribute;
+        }
+        public JsonObject getNestedJsonj() {
+            return nestedJsonj;
+        }
+        public void setNestedJsonj(JsonObject nestedJsonj) {
+            this.nestedJsonj = nestedJsonj;
+        }
+    }
+
+    public void shouldMixJsonjWithPojos() throws JsonProcessingException {
+        Foo foo = new Foo();
+        foo.setAttribute("Hi wrld");
+        foo.setAnotherAttribute(42);
+        JsonObject nested = object(
+            field("meaning_of_life",42),
+            field("a",42.0),
+            field("b",true),
+            field("c",array(42,"foo",3.14,true,null)),
+            field("d",object(field("a",1)))
+        );
+        foo.setNestedJsonj(nested);
+
+        String serialized = objectMapper.writeValueAsString(foo);
+        JsonObject object = parser.parseObject(serialized);
+        assertThat(object.getString("attribute")).isEqualTo("Hi wrld");
+        assertThat(object.getInt("anotherAttribute")).isEqualTo(42);
+        assertThat(object.getObject("nestedJsonj")).isEqualTo(nested);
     }
 }
